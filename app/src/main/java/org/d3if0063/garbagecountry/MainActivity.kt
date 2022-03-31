fa1b9e582ea0f91faa346e8c0233401ecf4b2e0f
prabuaddin <prabuaddin@student.telkomuniversity.ac.id>
2022-03-31 15:15:23 +0700

    Initial commit

---
+++ app/src/main/java/org/d3if0063/garbagecountry/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.d3if0063.garbagecountry
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
